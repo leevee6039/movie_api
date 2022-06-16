a736cce7fc9cde561f78cf92df123fa580fac782
leevee6039 <leevee6039@gmail.com>
2022-06-16 00:34:49 -0400

    fixed typo

--- index.js
+++ index.js
@@ -12,15 +12,15 @@ const express = require('express'),
 
 const { check, validationResult } = require('express-validator');
 
-mongoose.connect('mongodb://localhost:27017/myFlixDB', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-// mongoose.connect(process.env.CONNECTION_URI, {
+// mongoose.connect('mongodb://localhost:27017/myFlixDB', {
 //   useNewUrlParser: true,
 //   useUnifiedTopology: true,
 // });
+
+mongoose.connect(process.env.CONNECTION_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // create a write stream(in append mode)
 // 'a': Open file for appending. The file is created if it does not exist.
