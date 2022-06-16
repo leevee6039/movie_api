d8c0e55bd0f6f437ddb47deb08751fa951b021c8
leevee6039 <leevee6039@gmail.com>
2022-06-15 23:10:02 -0400

    fixed type 'USername' to 'Username'

--- passport.js
+++ passport.js
@@ -15,7 +15,7 @@ passport.use(
     },
     (username, password, callback) => {
       console.log(username + '  ' + password);
-      Users.findOne({ USername: username }, (error, user) => {
+      Users.findOne({ Username: username }, (error, user) => {
         if (error) {
           console.log(error);
           return callback(error);
